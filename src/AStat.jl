ddd59407755a5f5d6092f9ccb80eed9e962ad67e
BuildTools <unconfigured@null.spigotmc.org>
2024-01-02 21:53:33 -0500

    first cut

---
+++ src/AStat.jl
@@ -0,0 +1,61 @@
+module AStat
+
+export ols, lis
+
+function ols( X, y )
+    n = length(y)
+    inv = pinv(X'*X)
+    beta = inv*X'*y
+    yhat = X*beta
+    res = y - yhat
+    var = res'*res/n
+    yvar = y'*y/n
+    return Dict(
+        :beta => beta,
+        :var => var,
+        :yvar => yvar,
+        :std => sqrt(var),
+        :covbeta => var * inv,
+        :R2 => 1 - var/yvar,
+        :yhat => yhat,
+        :res => res,
+    )
+end
+
+function lis( x::AbstractVector{T} ) where {T}
+    n = length(x)
+    indices = fill( 0, n )
+    parents = fill( 0, n )
+    l = 0
+    for i = 1:n
+        r = binary_search( indices, x, x[i], l )
+        if r > l
+            l = r
+            indices[r] = i
+            
+        elseif x[i] < x[indices[r]]
+            indices[r] = i
+        end
+    end
+
+    return indices[1:l]
+end
+
+function binary_search( indices, a, x, n )
+    if n == 0
+        return 1
+    end
+    lo = 1
+    hi = n+1
+    while lo < hi
+        mid = div( lo + hi, 2 )
+        if x <= a[indices[mid]]
+            hi = mid
+        else
+            lo = mid+1
+        end
+    end
+    return lo
+end
+
+end # module AStat
