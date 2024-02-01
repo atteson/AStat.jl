54af68d52c7d13cde9b7d3dad9335b04f1e6f187
BuildTools <unconfigured@null.spigotmc.org>
2024-02-01 16:24:41 -0500

    CG seems to be able to generate rand IRV's

--- src/CG.jl
+++ src/CG.jl
@@ -3,68 +3,94 @@ export IndependentRandomVariable, Constant, open_interval
 
 using Distributions
 using Random
+using MissingTypes
 
+# subtype of Real so that they can be used as parameters of Distributions
 abstract type RandomVariable <: Real
 end
 
-struct IndependentRandomVariable{T <: Distribution} <: RandomVariable
+mutable struct Constant{T} <: RandomVariable
+    lo::T
+    left::Bool
+    hi::T
+    right::Bool
+    value::Vector{MissingType{T}}
+    visited::Bool
+end
+
+mutable struct IndependentRandomVariable{T <: Distribution, U <: Random.gentype(Random.gentype(T))} <: RandomVariable
     distribution::T
+    value::Vector{MissingType{U}}
+    visited::Bool
 end
 
-struct Constant{T} <: RandomVariable
-    lo::Vector{T}
-    left::Vector{Bool}
-    hi::Vector{T}
-    right::Vector{Bool}
+Constant() = Constant{Float64}( -Inf, false, Inf, false, MissingType{Float64}[MissingTypes.missing_value(Float64)], false )
+# needed for constructing Normal
+Constant{T}( x ) where T = ( x -> Constant{T}( x, true, x, true, convert( Vector{MissingType{T}}, [x] ), false ) )( convert( T, x ) )
+function open_interval( x, y )
+    (x,y) = promote(x,y)
+    T = typeof(x)
+    return Constant{T}( x, false, y, false, [MissingTypes.missing_value(T)], false )
 end
 
-Constant() = Constant( [-Inf], [false], [Inf], [false] )
-Constant{T}( x ) where T = ( x -> Constant( [x], [true], [x], [true] ) )(convert( T, x ))
-open_interval( x, y ) = ( (x, y) -> Constant( [x], [false], [y], [false] ) )(promote(x,y)...)
-Random.gentype( X::IndependentRandomVariable{T} ) where T = promote_type(Random.gentype.( params( X.distribution ) )...)
+function IndependentRandomVariable( distribution::Distribution )
+    T = Random.gentype(Random.gentype(distribution))
+    return IndependentRandomVariable( distribution, MissingType{T}[MissingTypes.missing_value(T)], false )
+end
+
 Random.gentype( c::Constant{T} ) where T = T
+Random.gentype( ::Type{Constant{T}} ) where T = T
+Random.gentype( X::IndependentRandomVariable{T} ) where T = promote_type(Random.gentype.( params( X.distribution ) )...)
 
-Base.:<( x::Constant{T}, y::Constant{T} ) where T = x.hi[end] < y.lo[end] || ( x.hi[end] == y.lo[end] && !( x.right[end] && y.left[end] ) )
+Base.:<( x::Constant{T}, y::Constant{T} ) where T = x.hi < y.lo || ( x.hi == y.lo && !( x.right && y.left ) )
 
-function Base.push!( x::Constant{T}, y::U ) where {T, U <: Number}
-    @assert( ( y < x.hi[end] || ( y == x.hi[end] && x.right[end] ) ) && ( y > x.lo[end] || ( y == x.lo[end] && x.left[end] ) ) )
-    push!( x.lo, y )
-    push!( x.left, true )
-    push!( x.hi, y )
-    push!( x.right, true )
+function Base.setindex!( x::Constant{T}, y::U ) where {T, U <: Number}
+    @assert( ( y < x.hi || ( y == x.hi && x.right ) ) && ( y > x.lo || ( y == x.lo && x.left ) ) )
+    x.value[1] = y
     return x
 end
 
-Base.pop!( x::Constant{T} ) where T = (pop!( x.lo ), pop!( x.left ), pop!( x.hi ), pop!( x.right ))
+function clear!( x::Constant{T} ) where T
+    x.visited = false
+end
 
-function generate_distribution( rng::AbstractRNG, distribution::Distribution;
-                                env::Dict{RandomVariable,AbstractArray{T}} = Dict{RandomVariable,AbstractArray{T}}() ) where T
-    generated_parmas = T[]
-    for param in params(distribution)
-        if !haskey( env, param )
-            env[param] = rand!(rng, Random.SamplerTrivial(param), env=env )
+function clear!( x::IndependentRandomVariable{T} ) where T
+    if x.visited
+        x.visited = false
+        for param in params(distribution)
+            clear!( param )
         end
-        push!( generated_params, env[param] )
+    end
+end
+
+function generate_distribution( rng::AbstractRNG, distribution::U ) where {U <: Distribution}
+    generated_params = Random.gentype(Random.gentype(distribution))[]
+    for param in params(distribution)
+        if !param.visited
+            param.visited = true
+            rand!(rng, param.value, Random.SamplerTrivial(param), clear=false )
+        end
+        push!( generated_params, param.value[1] )
     end
     return Base.typename(U).wrapper(generated_params...)
 end
 
-function Random.rand!(rng::AbstractRNG, A::AbstractArray{T}, sp::Random.SamplerTrivial{V};
-                      env::Dict{RandomVariable,AbstractArray{T}} = Dict{RandomVariable,AbstractArray{T}}() ) where
-    {T, U, V <: Constant{U}}
-    
-    c = sp.self
-    @assert( c.lo == c.hi && c.left && c.right )
-    return c.lo
+function Random.rand!(rng::AbstractRNG, A::AbstractArray{T}, sp::Random.SamplerTrivial{V}; clear::Bool = true ) where {T,V}
+    @assert( !ismissing( sp.self.value[1] ) )
+    A .= sp.self.value[1]
+    if clear
+        clear!( sp.self )
+    end
+    return A
 end
 
 function Random.rand!(rng::AbstractRNG, A::AbstractArray{T}, sp::Random.SamplerTrivial{V};
-                      env::Dict{RandomVariable,AbstractArray{T}} = Dict{RandomVariable,AbstractArray{T}}() ) where
-    {T, U, V <: IndependentRandomVariable{U}}
-
-                      
+                      clear::Bool = true ) where{T, U, V <: IndependentRandomVariable{U}}
     for index in eachindex(A)
-        A[index] = rand!( rng, generate_distribution( rng, sp.self.distribution, env=env ) )
+        A[index] = rand( rng, generate_distribution( rng, sp.self.distribution ) )
+    end
+    if clear
+        clear!( sp.self )
     end
     return A
 end
@@ -73,13 +99,10 @@ struct IIDRandomProcess{T <: Distribution}
     distribution::T
 end
 
-function Random.rand!(rng::AbstractRNG, A::AbstractArray{T}, sp::Random.SamplerTrivial{V};
-                      env::Dict{RandomVariable,AbstractArray{T}} = Dict{RandomVariable,AbstractArray{T}}() ) where
-    {T, U, V <: IIDRandomProcess{U}}
-
+function Random.rand!(rng::AbstractRNG, A::AbstractArray{T}, sp::Random.SamplerTrivial{IIDRandomProcess{U}} ) where {T, U}
     (n, s) = Iterators.peel(size(A))
     for i in CartesianIndices(([1:x for x in s]...,))
-        A[:,i] = rand!( rng, generate_distribution( rng, sp.self.distribution, env=env ), n )
+        A[:,i] = rand!( rng, generate_distribution( rng, sp.self.distribution ), n )
     end
     return A
 end
--- test/test_cg.jl
+++ test/test_cg.jl
@@ -14,23 +14,6 @@ Y = IndependentRandomVariable( Normal( μ₁, σ₁ ) )
 
 @assert( Random.gentype(Y) == Float32 )
 
+push!( μ₀, 0.0 )
+push!( σ₀, 1.0 )
 rand( X, 20 )
-
-import Random: default_rng, gentype, Sampler, typeof_rng
-@which rand( X, 10 )
-@which rand(X, Dims(10,))
-@which rand(default_rng(), X, Dims(10,))
-rng = r = default_rng()
-dims = Dims(10,)
-gentype(X)
-@which rand!(r, Array{gentype(X)}(undef, dims), X)
-A = Array{gentype(X)}(undef, dims)
-sp = Sampler(rng, X)
-@which rand!(rng, A, sp)
-@which rand(rng, sp)
-
-x = X
-r=Val(Inf)
-Sampler(typeof_rng(rng), x, r)
-
-rand!(rng, A, sp)
